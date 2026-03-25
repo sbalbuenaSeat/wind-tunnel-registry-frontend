5dcd86f2a5c00fad742808dbdfbde28e24f64964
sbalbuenaSeat <saray.balbuena@code.seat>
2026-03-25 15:15:42 +0100

    feat: update EntriesList

---
+++ src/components/Entries/EntriesSkeleton/EntriesSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from '@chakra-ui/react';
+import { EntriesCardSkeleton } from '@components/Entries/EntriesCardSkeleton/EntriesCardSkeleton.tsx';
+import styles from './EntriesSkeleton.module.css';
+
+export const EntriesSkeleton = () => {
+  return (
+    <div className={styles.entriesSkeletonContainer}>
+      <Skeleton height="32px" width="160px" borderRadius="md" />
+      <div className={styles.entriesGrid}>
+        <EntriesCardSkeleton />
+        <EntriesCardSkeleton />
+        <EntriesCardSkeleton />
+      </div>
+    </div>
+  );
+};
